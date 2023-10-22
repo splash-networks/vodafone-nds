e7233c28beda5f87586eb56d29724201fe9c3899
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2023-10-22 12:23:29 +0500

    15 questions

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="http://10.3.141.1/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
     <title>Survey</title>
 </head>
 <body>
